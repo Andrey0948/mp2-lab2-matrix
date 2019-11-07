40afc72a82f8ae1e32fcba7526630d7d86561442
Andrey0948 <andrey0948@mail.ru>
2019-11-07 20:39:57 +0300

    laba2

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -29,31 +29,31 @@ TEST(TVector, can_create_copied_vector)
   ASSERT_NO_THROW(TVector<int> v1(v));
 }
 
-TEST(TVector, copied_vector_is_equal_to_source_one)
+TEST(TVector, copied_vector_is_equal_to_source_one) // Скопированный вектор равен первому
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, copied_vector_has_its_own_memory)
+TEST(TVector, copied_vector_has_its_own_memory) // Скопированный вектор имеет собственную память
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_get_size)
+TEST(TVector, can_get_size) // Возможность получить вектор 
 {
   TVector<int> v(4);
 
   EXPECT_EQ(4, v.GetSize());
 }
 
-TEST(TVector, can_get_start_index)
+TEST(TVector, can_get_start_index) // Можем получить начальный индекс
 {
   TVector<int> v(4, 2);
 
   EXPECT_EQ(2, v.GetStartIndex());
 }
 
-TEST(TVector, can_set_and_get_element)
+TEST(TVector, can_set_and_get_element) // Можно установить и получить элемент
 {
   TVector<int> v(4);
   v[0] = 4;
@@ -61,93 +61,121 @@ TEST(TVector, can_set_and_get_element)
   EXPECT_EQ(4, v[0]);
 }
 
-TEST(TVector, throws_when_set_element_with_negative_index)
+TEST(TVector, throws_when_set_element_with_negative_index) // Ошибка при установке элемментас отрицательным индексом
+{
+	ASSERT_ANY_THROW(TVector<int> v(-5));
+}
+
+TEST(TVector, throws_when_set_element_with_too_large_index) // Ошибка при установке элемента с превышающим индексом
+{
+	ASSERT_ANY_THROW(TVector<int> v(10000000000000));
+}
+
+TEST(TVector, can_assign_vector_to_itself) 
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, throws_when_set_element_with_too_large_index)
+TEST(TVector, can_assign_vectors_of_equal_size) // Можно назначить векторы одинакового размера 
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vector_to_itself)
+TEST(TVector, assign_operator_change_vector_size) // Назначить оператор изменения размера вектора
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vectors_of_equal_size)
+TEST(TVector, can_assign_vectors_of_different_size) // Можно назначить векторы разного размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(5);
+	ASSERT_NO_THROW(v1 = v2);
 }
 
-TEST(TVector, assign_operator_change_vector_size)
+TEST(TVector, compare_equal_vectors_return_true) // сравнить разные вектора
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	v1[2] = 4;
+	v2[2] = 4;
+	ASSERT_TRUE(v1 == v2);
 }
 
-TEST(TVector, can_assign_vectors_of_different_size)
+TEST(TVector, compare_vector_with_itself_return_true) // Сравнить вектор с самим собой
 {
-  ADD_FAILURE();
+	TVector<int> v(4);
+	v[2] = 4;
+	ASSERT_TRUE(v == v);
 }
 
-TEST(TVector, compare_equal_vectors_return_true)
+TEST(TVector, vectors_with_different_size_are_not_equal) // Векторы разных размеров не равны
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(5);
+	ASSERT_FALSE(v1 == v2);
 }
 
-TEST(TVector, compare_vector_with_itself_return_true)
+TEST(TVector, can_add_scalar_to_vector) // Можно прибавить скаляр к вектору
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	for (int i = 0; i < 4; i++)
+		v1[i] = 1;
+	EXPECT_EQ(v1, v2 + 1);
 }
 
-TEST(TVector, vectors_with_different_size_are_not_equal)
+TEST(TVector, can_subtract_scalar_from_vector) // Можно вычесть скаляр из вектора
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	for (int i = 0; i < 4; i++)
+	{
+		v1[i] = 4;
+		v2[i] = 5;
+	}
+	EXPECT_EQ(v1, v2 - 1);
 }
 
-TEST(TVector, can_add_scalar_to_vector)
+TEST(TVector, can_multiply_scalar_by_vector) // Можно умножить скаляр на вектор
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	for (int i = 0; i < 4; i++)
+	{
+		v1[i] = 16;
+		v2[i] = 4;
+	}
+	EXPECT_EQ(v1, v2 * 4);
 }
 
-TEST(TVector, can_subtract_scalar_from_vector)
+TEST(TVector, can_add_vectors_with_equal_size) // Можно складывать векторы одинаковых размеров
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	ASSERT_NO_THROW(v1+v2);
 }
 
-TEST(TVector, can_multiply_scalar_by_vector)
+TEST(TVector, cant_add_vectors_with_not_equal_size) // Нельзя складывать векторы разных размеров
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(5);
+	ASSERT_ANY_THROW(v1+v2);
 }
 
-TEST(TVector, can_add_vectors_with_equal_size)
+TEST(TVector, can_subtract_vectors_with_equal_size) // Можно вычитать векторы одинаковых размеров
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	ASSERT_NO_THROW(v1-v2);
 }
 
-TEST(TVector, cant_add_vectors_with_not_equal_size)
+TEST(TVector, cant_subtract_vectors_with_not_equal_size) // Нельзя вычитать векторы разных размеров
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(5);
+	ASSERT_ANY_THROW(v1-v2);
 }
 
-TEST(TVector, can_subtract_vectors_with_equal_size)
+TEST(TVector, can_multiply_vectors_with_equal_size) // Можно умножать векторы одинаковых размеров 
 {
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(4);
+	ASSERT_NO_THROW(v1*v2);
 }
 
-TEST(TVector, cant_subtract_vectors_with_not_equal_size)
+TEST(TVector, cant_multiply_vectors_with_not_equal_size) // Нельзя умножать векторы разных размеров
 {
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_multiply_vectors_with_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, cant_multiply_vectors_with_not_equal_size)
-{
-  ADD_FAILURE();
+	TVector<int> v1(4), v2(5);
+	ASSERT_ANY_THROW(v1*v2);
 }
 
